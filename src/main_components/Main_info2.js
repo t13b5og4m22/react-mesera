4c99a3d8caa7bf77b5cc1c3adb8efc6e78cebb77
xtaphoney <itmo.khanil.kireev@gmail.com>
2022-12-12 15:54:28 +0300

    added mobile version 2

--- src/main_components/Main_info2.js
+++ src/main_components/Main_info2.js
@@ -6,10 +6,12 @@ function Main_info2() {
   return (
     <div className="Main_info2">
         <div className='container'>
-          <h3>Мы провели десятки туров и корпоративов на различных акваториях.</h3>
+          <div className='container__title'>
+            <h3>Мы провели десятки туров и корпоративов на различных акваториях.</h3>
+            <img src={star1} className='star star1'></img>
+            <img src={star2} className='star star2'></img>
+          </div>
           <p>Москва-река, Большой Строгинский<br/>зат., Химкинское вдхр.</p>
-          <img src={star1} className='star star1'></img>
-          <img src={star2} className='star star2'></img>
         </div>        
     </div>
   );
